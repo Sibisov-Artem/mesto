c7513c773f5dc9610157519cefef1a86773bdddf
Artem Sibisov <artsib7777@yandex.ru>
2023-02-20 22:32:53 +0800

    add function add/hide inputError

--- scripts/validate.js
+++ scripts/validate.js
@@ -7,3 +7,17 @@ const formsConfig = {
   inputErrorClass: 'popup__input_type_error',
   errorClass: 'popup__input-error'
 }
+// функция показа ошибки
+const showInputError = (formElement, inputElement, errorMessage) => {
+  const errorText = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.add(formsConfig.inputErrorClass);
+  errorText.textContent = errorMessage;
+  errorText.classList.add('form__input-error_active');
+};
+
+const hideInputError = (formElement, inputElement) => {
+  const errorText = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.remove(formsConfig.inputErrorClass);
+  errorText.classList.remove('form__input-error_active');
+  errorText.textContent = '';
+};
